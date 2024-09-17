18dfb0529e385398302a1771daeb7ab0f518a3dd
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-17 19:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+const squares=document.querySelectorAll(".square");
+// console.log(squares);
+squares.forEach(square=>{
+	// console.log(square);
+	square.addEventListener("mouseenter",function(){
+		squares.forEach(otherdiv=>{
+			if(otherdiv!==this){
+				otherdiv.style.backgroundColor='#6F4E37';
+			}
+		});
+	});
+	square.addEventListener("mouseleave",function(){
+		squares.forEach(otherdiv=>{
+			otherdiv.style.backgroundColor='#E6E6FA';
+		});
+	});
+});
